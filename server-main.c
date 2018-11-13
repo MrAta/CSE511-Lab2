0e70ceb88bcfcfa1728869c03914f632fd1e2e87
qkb7 <qkb5007@psu.edu>
2018-11-12 22:05:34 -0500

    test

--- server-main.c
+++ server-main.c
@@ -2,27 +2,28 @@
 // Created by sps5394 on 10/18/18.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 #include "server-part1.h"
-#include "server-part2.h"
-#include "server-part3.h"
+// #include "server-part2.h"
+// #include "server-part3.h"
 
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("Must provide value for what sort of server to run\n");
-    return  1;
-  }
-  switch(atoi(argv[1])) {
-    case 1:
-      return run_server_1();
-    case 2:
-      return run_server_2();
-    case 3:
-      return run_server_3();
-    default:
-      printf("Illegal argument value. Exiting\n");
-      return 1;
-  }
+  // if (argc < 2) {
+  //   printf("Must provide value for what sort of server to run\n");
+  //   return  1;
+  // }
+  // switch(atoi(argv[1])) {
+  //   case 1:
+  //     return run_server_1();
+  //   case 2:
+  //     return run_server_2();
+  //   case 3:
+  //     return run_server_3();
+  //   default:
+  //     printf("Illegal argument value. Exiting\n");
+  //     return 1;
+  // }
+  run_server_1();
 }
